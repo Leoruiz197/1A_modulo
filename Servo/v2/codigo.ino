ac70d0f96ec5efb72eb2ec615058585b5030b7ac
Leo Ruiz <ruizruizleo@hotmail.com>
2022-05-16 22:03:45 -0300

    Add files via upload

---
+++ Servo/v2/codigo.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+
+Servo s;
+int pos;
+
+const int bot1 = 2;
+const int bot2 = 3;
+
+
+
+void setup() {
+  pinMode(bot1, INPUT);
+  pinMode(bot2, INPUT);
+  
+  s.attach(9);
+  Serial.begin(9600);
+  s.write(160);
+}
+
+int var1;
+int var2;
+
+void loop() {
+  var1 = digitalRead(bot1);
+  var2 = digitalRead(bot2);
+  
+  if(var1){
+
+    for(pos = 100; pos < 160; pos++)
+    {
+      s.write(pos);
+      delay(15);
+    }
+  }
+
+  if(var2){
+
+    for(pos = 160; pos > 100; pos--)
+    {
+      s.write(pos);
+      delay(15);
+    }
+  }
+}
